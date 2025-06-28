c9bb102818b7c156a0ec4100b63e3a4ea878e97b
Sahar  Iqbal <120391110+sahariq@users.noreply.github.com>
2025-06-28 23:53:11 +0500

    admin panel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,13 @@ export default defineConfig({
   },
   preview: {
     port: 5432
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      }
+    }
   }
 }) 
